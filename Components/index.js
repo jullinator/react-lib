a8e627f0035303936b6f095ef81cb798b7d3cb0c
jullinator <justus.karlsson@hotmail.se>
2016-09-12 02:31:16 +0200

    bugs

--- Components/index.js
+++ Components/index.js
@@ -1,24 +1,21 @@
 import {Text, View, TextInput, TouchableOpacity } from 'react-native'
 import React, {Component} from 'react'
+import style from '../Styles'
 
-export const Field = ({update, name="..."})=>
-  <TextInput placeholder={name} onChangeText={(text)=>{update(text)} />
+export const Field = ({stil="",update, name="..."})=>
+  <TextInput {...style(stil)} placeholder={name} onChangeText={(text)=>{update(text)}} />
 
-export const Button = ({onPress, text="Button"})=>
-  <TouchableOpacity onPress={onPress} >
-    <Text>{text} </Text>
+export const Button = ({stil1="",stil2="",onPress, text="Button"})=>
+  <TouchableOpacity {...style(stil1)} onPress={onPress} >
+    <Text  {...style(stil2)} >{text} </Text>
   </TouchableOpacity>
 
 
-export const Linker = ({text, path, size="12"}) =>
-		<Button onPress={Actions. size={size} text={text} />
-
-
-export const Text = ({text})=>
-	<Text style={{fontSize:'20px'}}>
+export const Txt = ({stil="",text})=>
+	<Text {...style(stil)} >
 		{text}
 	</Text>
-	
+
 
 
 // export const MultiField = ({update,desc, size="12"})=>{
@@ -30,6 +27,3 @@ export const Container = ({children})=>
 	<View {...style('container')} >
 		{children}
 	</View>
-
-
-
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import AppStore from './Mobx'
 import Router from './Router'
 import Launch from './Scenes'
+import React from 'react'
 
 const store = new AppStore()
 export default ()=>
