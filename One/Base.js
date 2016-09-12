5eb0a47a3dce0bb1eb1ad237fceb26776b028c60
jullinator <justus.karlsson@hotmail.se>
2016-09-12 06:30:15 +0200

    Update Base.js

--- One/Base.js
+++ One/Base.js
@@ -1,1 +1,16 @@
 
+export default class {
+  @observable comp ={
+    button:function(  ){  return(<TouchableOpacity style={this.style[key]}  /> )}
+  }
+  
+  constructor(ref){
+    this.db = Database.ref(ref) 
+  }
+  fetch(callback){
+    this.db.on('value',snap=>callback(snap))
+  }
+  save(){
+    this.db.push(this.data)
+  }
+}
